dce85daa914df24577732e9929fb3c3ad05a021e
julienrat <julienrat@riseup.net>
2018-12-13 15:13:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,16 +5,6 @@ import dmx
 sender = dmx.DMX_Serial("/dev/ttyUSB0")
   
 sender.start()
-sender.set_data(255,6)
-while True:
-    for u in range (0,255):
-        sender.set_data(u,4)
-        time.sleep(0.005)
-    sender.set_data(105,5)
-    time.sleep( 0.5)
-    for u in range (0,255):
-        sender.set_data(255-u,4)
-        time.sleep(0.005)
-    sender.set_data(0,5)
-    #sender.set_data(0,4)
-    time.sleep( 0.5)
+sender.set_data(255,6) #channel 6 Full ON 255
+time.sleep(5)
+sender.set_data(0,6) #channel 6 Full OFF 0
